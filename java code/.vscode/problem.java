e7ac87154d5f08bcc8891bdceb5afcf9336fe852
fulihua <0346999541@qq.com>
2026-03-04 14:13:48 +0800

    总结了常见的错误 3月4日

---
+++ java code/.vscode/problem.java
@@ -0,0 +1,24 @@
+package .vscode;
+
+public class problem {
+
+    /*
+    写Java代码时的常见错误：
+    1.没有在终端中同步：要确保在终端中当前的目录中有这么文件夹，
+    迫不得已时需要手动编译：# 1. 确保当前目录有 ThreadTest.java
+ls ThreadTest.java
+
+# 2. 编译（生成 .class 文件）
+javac ThreadTest.java
+
+# 3. 运行主类（不加 .class 后缀）
+java ThreadTest
+
+    2.出现Unresolved compilation problem，说明编译阶段就失败了。
+    一般需要添加包声明，在文件最开头（所有代码之前）加上：
+
+    package dayxx;
+
+    */
+    
+}
